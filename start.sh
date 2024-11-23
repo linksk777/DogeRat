c505744e6166fc4e0b98837a91a114e95520ec92
linksk777 <l.inksk777@gmail.com>
2024-11-23 14:00:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -66,8 +66,8 @@ else
     rm node_modules.zip
 fi
 
-read -p "Enter your bot token: " token
-read -p "Enter your chat ID: " id
+read -p "Enter your bot token: " 6185982181:AAHm9_sc9YwnRc7szhGkZsSXoZ4XfGZwlcA
+read -p "Enter your chat ID: " 1042029807
 sed -i "s/const token = 'your token here'/const token = '$token'/g" index.js
 sed -i "s/const id = 'chat id here'/const id = '$id'/g" index.js
 echo "Server uploaded successfully! Now open new tab and follow rest instructions"
